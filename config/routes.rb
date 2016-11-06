9b0ed0f0d5c55fdeb08278fe80250daa7e530f6a
ccflack <ccflack@me.com>
2016-11-06 18:16:16 -0500

    Added tester api_tokens to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
-# My api_key: 9HKAppkEVtgHeCFuyruHeQXr
+# My local api_key: 9HKAppkEVtgHeCFuyruHeQXr
+# heroku api_key: b6PmbuZkCzHmqXyLLjoPoMmp
